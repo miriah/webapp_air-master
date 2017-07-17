b20feabeab2f8a096c7258b219893cda111572f5
Aspen Hopkins <aspenhopkins@aspenhopkinsmecom.local>
2017-07-17 15:39:35 -0600

    initial commit with basic map functionality regarding leaflet, and on-click functionality for google maps +d3 visualization of air quality from data base

---
+++ leaflet-tutorials/map_build.js
@@ -0,0 +1,19 @@
+// initialize the map
+var map = L.map('map').setView([40.7608, -111.8910], 13);
+// load a tile layer
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1Ijoic2tpdHJlZSIsImEiOiJjajUyb2l0YzQwaHJwMnFwMTNhdGwxMGx1In0.V5OuKXRdmwjq4Lk3o8me1A', {
+  attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+  maxZoom: 18,
+  id: 'mapbox.streets',
+  accessToken: 'pk.eyJ1Ijoic2tpdHJlZSIsImEiOiJjajUydDkwZjUwaHp1MzJxZHhkYnl3eTd4In0.TdQB-1U_ID-37stKON_osw'
+}).addTo(map);
+
+var markr = null;
+
+function onMapClick(e) {
+  markr = new L.marker(e.latlng)
+  .addTo(map)
+  .bindPopup(e.latlng.toString())
+}
+
+map.on('click', onMapClick);
